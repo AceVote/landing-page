f1114ea1509b72bbf5cc63a7cee093b19b21c130
njokuScript <kalunjoku123@gmail.com>
2020-03-17 07:57:20 +0100

    finished working on teeam data

--- src/pages/team.js
+++ src/pages/team.js
@@ -8,7 +8,8 @@ const team = [
     role: "Blockchain Engineer",
     githubLink: "https://www.github.com/Khay-EMMA",
     linkedinLink: "https://www.github.com/Khay-EMMA",
-    twitterLink: "https://www.twitter.com/njokuScript"
+    twitterLink: "https://www.twitter.com/njokuScript",
+    objectID: 1
   },
   {
     name: "Naheem Noah",
@@ -16,7 +17,8 @@ const team = [
     role: "Business Developer",
     githubLink: "https://www.github.com/Khay-EMMA",
     linkedinLink: "https://www.github.com/Khay-EMMA",
-    twitterLink: "https://www.twitter.com/njokuScript"
+    twitterLink: "https://www.twitter.com/njokuScript",
+    objectID: 2
   },
   {
     name: "Akorede",
@@ -24,7 +26,8 @@ const team = [
     role: "UI-UX Designer",
     githubLink: "https://www.github.com/Khay-EMMA",
     linkedinLink: "https://www.github.com/Khay-EMMA",
-    twitterLink: "https://www.twitter.com/njokuScript"
+    twitterLink: "https://www.twitter.com/njokuScript",
+    objectID: 3
   },
   {
     name: "Anthony",
@@ -32,64 +35,78 @@ const team = [
     role: "Android Engineer",
     githubLink: "https://www.github.com/Khay-EMMA",
     linkedinLink: "https://www.github.com/Khay-EMMA",
-    twitterLink: "https://www.twitter.com/njokuScript"
+    twitterLink: "https://www.twitter.com/njokuScript",
+    objectID: 4
   }
 ];
 const Team = () => {
   return (
-    <section className="section-spacer team-section">
-      <div className="container">
-        <header className="text-center section-header">
-          <h2 className="section-title">Meet The AceVote Team</h2>
-          <p className="section-subtitle">
-            Passionate Entrepreuners driving the success of AceVote.
-          </p>
-        </header>
-        <div className="container-fluid">
-          <div
-            id="carouselExample"
-            className="carouselPrograms carousel slide"
-            data-ride="carousel"
-            data-interval="false"
-          >
-            <div className="carousel-inner row w-100 mx-auto" role="listbox">
-              <div className="carousel-item col-md-4 col-15 active">
-                <div className="card event-card">
-                  <div className="card hovercard">
-                    <div className="cardheader"></div>
-                    <div className="avatar">
-                      <img src={avatar} alt="lead avatar" />
-                    </div>
-                    <div className="info">
-                      <div className="title">
-                        <h5>Name goes here</h5>
-                        <p>Lead Organizer</p>
+    <>
+      <section className="section-spacer team-section">
+        <div className="container">
+          <header className="text-center section-header">
+            <h2 className="section-title">Meet The AceVote Team</h2>
+            <p className="section-subtitle">
+              Passionate Entrepreuners driving the success of AceVote.
+            </p>
+          </header>
+          {team.map(item => {
+            return (
+              <div key={item.objectID} className="container-fluid">
+                <div
+                  id="carouselExample"
+                  className="carouselPrograms carousel slide"
+                  data-ride="carousel"
+                  data-interval="false"
+                >
+                  <div
+                    className="carousel-inner row w-100 mx-auto"
+                    role="listbox"
+                  >
+                    <div className="carousel-item col-md-4 col-15 active">
+                      <div className="card event-card">
+                        <div className="card hovercard">
+                          <div className="cardheader"></div>
+                          <div className="avatar">
+                            <img src={avatar} alt="lead avatar" />
+                          </div>
+                          <div className="info">
+                            <div className="title">
+                              <h5>{item.name}</h5>
+                              <p>{item.title}</p>
+                            </div>
+                            <div className="desc">{item.role}</div>
+                          </div>
+                          <div className="bottom">
+                            <ul className="social-list__inline mt-10">
+                              <button type="button">
+                                <li>
+                                  <FaTwitter />
+                                </li>
+                              </button>
+                              <button>
+                                <li>
+                                  <FaGithub />
+                                </li>
+                              </button>
+                              <button>
+                                <li>
+                                  <FaLinkedin />
+                                </li>
+                              </button>
+                            </ul>
+                          </div>
+                        </div>
                       </div>
-                      <div className="desc">Mobile and Web developer</div>
-                      <div className="desc">Open source enthusiast</div>
-                      <div className="desc">Community mentor</div>
-                    </div>
-                    <div className="bottom">
-                      <ul className="social-list__inline mt-10">
-                        <li>
-                          <FaTwitter />
-                        </li>
-                        <li>
-                          <FaGithub />
-                        </li>
-                        <li>
-                          <FaLinkedin />
-                        </li>
-                      </ul>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
